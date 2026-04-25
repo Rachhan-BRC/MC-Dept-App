d4b579b33978b0035c667bf8f8bcd8a3b5178dee
PanharithSor16 <panharithsor16@gmail.com>
2026-04-25 13:21:48 +0700

    new function DefectFlow

---
+++ MCReportTrackingResult/EditDefectOutFlow.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace MachineDeptApp.MCReportTrackingResult
+{
+    partial class EditDefectOutFlow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblCategory = new System.Windows.Forms.Label();
+            this.lblType = new System.Windows.Forms.Label();
+            this.lblMonth = new System.Windows.Forms.Label();
+            this.lblQty = new System.Windows.Forms.Label();
+            this.tbQty = new System.Windows.Forms.TextBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblCategory
+            // 
+            this.lblCategory.AutoSize = true;
+            this.lblCategory.Font = new System.Drawing.Font("Khmer OS Battambang", 9F);
+            this.lblCategory.Location = new System.Drawing.Point(41, 9);
+            this.lblCategory.Name = "lblCategory";
+            this.lblCategory.Size = new System.Drawing.Size(66, 22);
+            this.lblCategory.TabIndex = 14;
+            this.lblCategory.Text = "Category :";
+            // 
+            // lblType
+            // 
+            this.lblType.AutoSize = true;
+            this.lblType.Font = new System.Drawing.Font("Khmer OS Battambang", 9F);
+            this.lblType.Location = new System.Drawing.Point(41, 39);
+            this.lblType.Name = "lblType";
+            this.lblType.Size = new System.Drawing.Size(44, 22);
+            this.lblType.TabIndex = 15;
+            this.lblType.Text = "Type :";
+            // 
+            // lblMonth
+            // 
+            this.lblMonth.AutoSize = true;
+            this.lblMonth.Font = new System.Drawing.Font("Khmer OS Battambang", 9F);
+            this.lblMonth.Location = new System.Drawing.Point(41, 69);
+            this.lblMonth.Name = "lblMonth";
+            this.lblMonth.Size = new System.Drawing.Size(49, 22);
+            this.lblMonth.TabIndex = 16;
+            this.lblMonth.Text = "Month :";
+            // 
+            // lblQty
+            // 
+            this.lblQty.AutoSize = true;
+            this.lblQty.Font = new System.Drawing.Font("Khmer OS Battambang", 9F);
+            this.lblQty.Location = new System.Drawing.Point(41, 106);
+            this.lblQty.Name = "lblQty";
+            this.lblQty.Size = new System.Drawing.Size(36, 22);
+            this.lblQty.TabIndex = 17;
+            this.lblQty.Text = "Qty :";
+            // 
+            // tbQty
+            // 
+            this.tbQty.Font = new System.Drawing.Font("Khmer OS Battambang", 9F);
+            this.tbQty.Location = new System.Drawing.Point(125, 103);
+            this.tbQty.Name = "tbQty";
+            this.tbQty.Size = new System.Drawing.Size(200, 30);
+            this.tbQty.TabIndex = 18;
+            // 
+            // btnSave
+            // 
+            this.btnSave.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(70)))), ((int)(((byte)(130)))), ((int)(((byte)(180)))));
+            this.btnSave.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSave.Font = new System.Drawing.Font("Khmer OS Battambang", 9F, System.Drawing.FontStyle.Bold);
+            this.btnSave.ForeColor = System.Drawing.Color.White;
+            this.btnSave.Location = new System.Drawing.Point(125, 149);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(90, 32);
+            this.btnSave.TabIndex = 19;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = false;
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(200)))), ((int)(((byte)(200)))), ((int)(((byte)(200)))));
+            this.btnCancel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCancel.Font = new System.Drawing.Font("Khmer OS Battambang", 9F);
+            this.btnCancel.Location = new System.Drawing.Point(229, 149);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(90, 32);
+            this.btnCancel.TabIndex = 20;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = false;
+            // 
+            // EditDefectOutFlow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(348, 205);
+            this.Controls.Add(this.lblCategory);
+            this.Controls.Add(this.lblType);
+            this.Controls.Add(this.lblMonth);
+            this.Controls.Add(this.lblQty);
+            this.Controls.Add(this.tbQty);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnCancel);
+            this.Name = "EditDefectOutFlow";
+            this.Text = "EditDefectOutFlow";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblCategory;
+        private System.Windows.Forms.Label lblType;
+        private System.Windows.Forms.Label lblMonth;
+        private System.Windows.Forms.Label lblQty;
+        private System.Windows.Forms.TextBox tbQty;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnCancel;
+    }
+}
